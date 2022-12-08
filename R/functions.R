da82dca452144560d72fe2a46b397bc6500d2c08
Daniel Cervone <daniel.cervone@gmail.com>
2022-12-08 10:31:40 +0100

    Source lipidomics data and created function for descriptive stats to source and run in Lesson code chunk.

---
+++ R/functions.R
@@ -0,0 +1,9 @@
+#' Calculate descriptive stats for each metabolite
+#' @param data Lipidomics dataset
+#' @return A data.frame/tibble
+descriptive_stats <- function(data) {
+  data %>%
+    dplyr::group_by(metabolite) %>%
+    dplyr::summarise(across(value, list(mean = mean, sd = sd))) %>%
+    dplyr::mutate(across(where(is.numeric), round, digits = 1))
+}
